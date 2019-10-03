677d225b5d83ab72bf0b7733635accb87b0c41f3
Nicole Antonacci <nicole.antonacci@gmail.com>
2019-10-02 18:19:52 -0700

    Updated react to 16.9. Added Dashboard test imports.

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,6 @@
 // Test away
+import React from 'react';
+import * as rtl from 'react-testing-library';
+import 'jest-dom/extend-expect';
+import Dashboard from './Dashboard.js';
+afterEach(rtl.cleanup);
